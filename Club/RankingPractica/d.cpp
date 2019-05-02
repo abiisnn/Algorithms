293542d59981b4f42e801f5e27c39fc87020e8f0
abiisnn <abigail.nic.say@hotmail.com>
2019-05-02 11:57:48 -0500

    d Update

--- Club/RankingPractica/d.cpp
+++ Club/RankingPractica/d.cpp
@@ -1,36 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+long long cuentaImpar(vector<long long> v) {
+	long long contador = 0;
+	for(int i = 0; i < v.size; i ++) {
+		if(v[i] && 1 && (v[i] != -1))
+			contador++;
+	}
+	return contador;
+}
+long long cuentaPar(vector<long long> v) {
+	long long contador = 0;
+	for(int i = 0; i < v.size; i ++) {
+		if(!(v[i] && 1) && (v[i] != -1))
+			contador++;
+	}
+	return contador;
+}
+
+vector <long long> t; 
+	
 int main () {
-	long long tesoro, llave, aux, suma, contador;
+	long long contador, tesoro, llave, aux;
 	cin >> tesoro >> llave;
-	vector <long long> t; 
 	vector <long long> l;
+	int isPar = 1;
+	long long impar = 0, par = 0;
 
 	// Recibir datos
 	for(int i = 0; i < tesoro; i++) {
-		cin >> aux;		
+		cin >> aux;
 		t.push_back(aux);
 	}
 	for(int j = 0; j < llave; j++) {
 		cin >> aux;
 		l.push_back(aux);
-	}	
+	}
 
 	contador = 0;
-	for(int i = 0; i < l.size(); i++) {
-		if(l[i] != -1) {
-			for(int j = 0; j < t.size(); j++) {
-				if(t[j] != -1) {			
-					suma = l[i] + t[j];
-					if(suma & 1) {
-						l[i] = -1;
-						t[j] = -1;					
-						contador++;		
-					}	
-				} // if
-				suma = 0;		
-			} // for
-		} // if
-	} // for
+	for(int i = 0 ; i < l.size(); i ++) {
+		if(l[i] && 1) {
+			impar = cuentaImpar(t);
+
+		}
+		impar = cuentaImpar(t);
+		par = cuentaPar(t);
+	}
 	cout << contador << endl;
 }
