2c8f59d61f3da4c6ea6c71d4432ea54ba46ef3d0
INFORMATICA <abigail.nic.say@hotmail.com>
2019-06-25 12:00:06 -0500

    Old

---
+++ CompetitiveProgramming3/Start/costCutting.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define optimizar_io ios_base::sync_with_stdio(0); cin.tie(0);
+
+int main() {
+	int i, j, number, tc;
+	cin >> tc;
+	for(i = 0; i < tc; i++ ) {
+		vector<int> v;
+		for(j = 0; j < 3; j++) {
+			cin >> number;
+			v.push_back(number);
+		}
+		sort(v.begin(), v.end());
+		cout << "Case " << i+1 << ": " << v[1] << endl;
+	}
+}
---
+++ CompetitiveProgramming3/Start/divisionNlogonia.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define optimizar_io ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+typedef long long ll;
+
+int main() {
+	ll a, b, x, y;
+	int tc, i;
+
+	while(scanf("%d", &tc), tc) {
+		cin >> x >> y;
+		for(i = 0; i < tc; i++) {
+			cin >> a >> b;
+			if((a == x) && (b > y)) {
+				cout << "divisa" << endl;
+			} else if((a > x) && (b == y)) {
+				cout << "divisa" << endl;
+			} else if((a == x) && (b < y)) {
+				cout << "divisa" << endl;
+			} else if((a < x) && (b == y)) {
+				cout << "divisa" << endl;
+			} else if((a == x) && (b == y)) {
+				cout << "divisa" << endl;
+			} else if((a > x) && (b > y)) {
+				cout << "NE" << endl;
+			} else if((a < x) && (b > y)) {
+				cout << "NO" << endl;
+			} else if((a < x) && (b < y)) {
+				cout << "SO" << endl;
+			} else if((a > x) && (b < y)) {
+				cout << "SE" << endl;
+			}
+		}
+	}
+} // main
