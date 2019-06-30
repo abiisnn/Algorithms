3cdbf8a176eb20de666dae0e63faff51471b0d4f
INFORMATICA <abigail.nic.say@hotmail.com>
2019-06-30 00:18:00 -0500

    The last

--- CompetitiveProgramming3/Introduction/Getting Started The Easy Problems/Easy/summingDigits.cpp
+++ CompetitiveProgramming3/Introduction/Getting Started The Easy Problems/Easy/summingDigits.cpp
@@ -5,11 +5,24 @@ using namespace std;
 #define optimizar_io ios_base::sync_with_stdio(0); cin.tie(0);
 typedef long long ll;
 
+ll getNumber(char c) {
+	return c - 48;
+}
 int main() {
-	ll number;
+	ll n, i;
+	char c;
+	string number;
 	cin >> number;
-	while(number > 9) {
-		
+	n = 0;
+	while(number.compare("0")) {
+		while(number.length() > 1) {
+			for(i = 0; i < number.length(); i++) {
+				n += getNumber(number.at(i));
+			}
+			number = to_string(n);
+			n = 0;
+		}		
+		cout << number << endl;
+		cin >> number;
 	}
-	cout << number << endl;
 }
