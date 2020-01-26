13d05ca7c37428fb5fefc6db024de685d84526f6
abiisnn <abigail.nic.say@hotmail.com>
2020-01-26 17:12:00 -0600

    ++

--- Codeforces/#575Div3/B.cpp
+++ Codeforces/#575Div3/B.cpp
@@ -4,14 +4,18 @@ using namespace std;
 #define optimizar_io ios_base::sync_with_stdio(0); cin.tie(0);
 #define fore(i, a, b) for(int i = a; i < b; i++)
 #define fori(i, a, b) for(int i = a; i <= b; i++)
-#define endl '\n'
 #define pb push_back
+#define mk make_pair
+#define f first
+#define s second
+#define endl '\n'
+
 
 typedef long long int lli;
 
 int main() {
 	optimizar_io
-	int i, k, n, q, aux;
+	int k, n, q, aux;
 	cin >> q;
 	while(q--) {
 		cin >> n >> k;
