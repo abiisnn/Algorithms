a4c304d44caffb0c7d95a2ee0b8bf7ffd2fcd3a5
abiisnn <abigail.nic.say@hotmail.com>
2020-07-06 22:02:57 -0500

    Upsolving div 3, problem D

--- Codeforces/Div3/#611Div3/D.cpp
+++ Codeforces/Div3/#611Div3/D.cpp
@@ -11,62 +11,49 @@ using namespace std;
 #define se second
 #define endl '\n'
 #define MAX 60
-#define INF 3ll * 1e9
+#define LIM 2 * 10e9
 typedef long long int lli;
 typedef long double ld;
+typedef pair<lli, lli> plli;
 const double PI = acos(-1.0);
 ld eps = 1e-9;
 
+lli total = 0;
+struct compare { 
+    constexpr bool operator()(plli const& a, plli const& b) const noexcept { return a.first > b.first; }
+};
+priority_queue<plli, vector<plli>, compare> opt; // Fi: distance, Se: pos
+set<lli> pos;
+
+void addElement(lli index, lli dis, int &cont) {
+	if(!pos.count(index) && index <= LIM && index >= -LIM) {
+		total += dis;
+		opt.push(mk(dis, index));
+		pos.insert(index);
+		cont++;
+	}
+}  
 int main() {
 	optimizar_io
 	int n, m; cin >> n >> m;
-	vector<lli> tree(n);
-	set<lli> places, t;
-	fore(i, 0, n) cin >> tree[i], t.insert(tree[i]);
+	set<lli> tree;
+	lli t;
+	fore(i, 0, n) {
+		cin >> t;
+		opt.push(mk(0, t));
+		pos.insert(t); tree.insert(t);
+	}
 	
-	sort(all(tree));
-
-	cout << "Arboles:" << endl;
-	fore(i, 0, n) cout << tree[i] << " ";
-	cout << endl;
-
-	fore(i, 0, n) {
-		lli l = tree[i] - 1;
-		lli r = tree[i] + 1;
-		if(!places.count(l) && !t.count(l)) {
-			places.insert(l);
-		}
-		if(!places.count(r) && !t.count(r)) {
-			places.insert(r);
-		}
+	int cont = 0;
+	while(1) {
+		plli a = opt.top(); opt.pop();
+		lli l = a.se - 1, r = a.se + 1, dis = a.fi + 1;
+		if(cont < m) addElement(l, dis, cont); else break;
+		if(cont < m) addElement(r, dis, cont); else break;
 	}
-
-	// Create set ans, pair: difference, treeindex
-	lli best = 0;
-	set<pair<lli, int>> sder, sizq;
-	fore(i, 0, n) {
-		lli dif = INF;
-		// Izquierda
-		lli next = tree[i]-1;
-		if(next >= izq[i]) {
-			dif = 1ll;
-		}
-		sizq.insert(mk(dif, i));
-		dif = INF;
-		next = tree[i] + 1;
-		if(next <= der[i]) {
-			dif = 1ll;
-		}
-		sder.insert(mk(dif, i));
+	cout << total << endl;
+	for(auto &i: pos) {
+		if(!tree.count(i)) cout << i << " ";
 	}
-	cout << "derecha: " << endl;
-	for(auto &i: sder) {
-		cout <<i.fi << " " << i.se << endl;
-	}
-	cout << "i<quierda: " << endl;
-	for(auto &i: sizq) {
-		cout <<i.fi << " " << i.se << endl;
-	}
-
+	cout << endl;	
 }
-
