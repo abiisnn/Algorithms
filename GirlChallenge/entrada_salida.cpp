eedc0a54616b2dad27361eb36b566cbd3bda13d9
abiisnn <abigail.nic.say@hotmail.com>
2020-08-04 20:58:03 -0500

    Vjudge Guide

---
+++ GirlChallenge/entrada_salida.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h> // Library
+using namespace std;
+
+int main() {
+	int a;
+	string b;
+	float c;
+	double d;
+	long int e;
+
+	// Read from console
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> d;
+	cin >> e;
+	
+	// Print data
+	cout << a << '\n';
+	cout << b << '\n';
+	cout << c << endl; // endl = \n
+	cout << d << endl;
+	cout << e << endl;
+}
